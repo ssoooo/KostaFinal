4c4a56d4fc6d6c3c3ea5d89de8192878fe80a64e
yangheesoo <gmltn4515@naver.com>
2017-11-24 12:30:20 +0900

    3

--- HelloSpring/src/hello/MessageBeanEng.java
+++ HelloSpring/src/hello/MessageBeanEng.java
@@ -5,6 +5,7 @@ public class MessageBeanEng implements MessageBean{
 	@Override
 	public void sayHello(String name) {
 		System.out.println("hello111111"+name);
+		System.out.println("123123");
 		
 	}
 
